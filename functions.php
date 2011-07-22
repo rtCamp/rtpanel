fc81d35d87224cfac5ec15673abb835583608f14
Manish Songirkar <manish.songirkar@rtcamp.com>
2011-07-22 13:02:22 +0000

    added browser and os detection code

--- functions.php
+++ functions.php
@@ -62,3 +62,50 @@ require_once( get_template_directory() . "/admin/rtp-theme-options.php" );
 //</YourMembership>',
 //        );
 //print_R(wp_remote_post('https://api.yourmembership.com', $args));
+
+// Nofollow external links only
+add_filter( 'the_content', 'rt_nofollow' );
+add_filter( 'the_excerpt', 'rt_nofollow' );
+function rt_nofollow( $content ) {
+    return preg_replace_callback( '/<a[^>]+/', 'rt_nofollow_callback', $content );
+}
+function rt_nofollow_callback( $matches ) {
+    $link = $matches[0];
+    $site_link = get_bloginfo( 'url' );
+    if ( strpos( $link, 'rel' ) === false ) {
+        $link = preg_replace( "%(href=\S(?!$site_link))%i", 'rel="nofollow" $1', $link );
+    } elseif ( preg_match( "%href=\S(?!$site_link)%i", $link ) ) {
+        $link = preg_replace( '/rel=\S(?!nofollow)\S*/i', 'rel="nofollow"', $link );
+    }
+    return $link;
+}
+
+/* 
+ * Browser Decection code and apply class to <?php body_class(); ?>
+ * Ref: http://wpsnipp.com/index.php/functions-php/browser-detection-and-os-detection-with-body_class/?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+wpsnipp+(wpsnipp+-+wordpress+code+snippets)&utm_content=Google+Reader
+ */
+function rt_body_class( $classes ) {
+    global $is_lynx, $is_gecko, $is_IE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone;
+    if ( $is_lynx ) $classes[] = 'lynx';
+    elseif ( $is_gecko ) $classes[] = 'gecko';
+    elseif ( $is_opera ) $classes[] = 'opera';
+    elseif ( $is_NS4 ) $classes[] = 'ns4';
+    elseif ( $is_safari ) $classes[] = 'safari';
+    elseif ( $is_chrome ) $classes[] = 'chrome';
+    elseif ( $is_IE ) {
+        $classes[] = 'ie';
+        if ( preg_match( '/MSIE ([0-9]+)([a-zA-Z0-9.]+)/', $_SERVER['HTTP_USER_AGENT'], $browser_version ) ) {
+            $classes[] = 'ie'.$browser_version[1];
+        }
+    } else $classes[] = 'unknown';
+    if ( $is_iphone ) $classes[] = 'iphone';
+    if ( stristr( $_SERVER['HTTP_USER_AGENT'], "mac") ) {
+        $classes[] = 'osx';
+    } elseif ( stristr( $_SERVER['HTTP_USER_AGENT'],"linux") ) {
+        $classes[] = 'linux';
+    } elseif ( stristr( $_SERVER['HTTP_USER_AGENT'],"windows") ) {
+        $classes[] = 'windows';
+    }
+    return $classes;
+}
+add_filter( 'body_class', 'rt_body_class' );
