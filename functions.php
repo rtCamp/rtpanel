eddd9143c1cc2b98f2b4a495d3c4c49144218129
Joshua Abenazer <joshua.abenazer@rtcamp.com>
2011-07-25 02:41:30 +0000

    Minor Change

--- functions.php
+++ functions.php
@@ -46,23 +46,6 @@ foreach( glob ( get_template_directory() . "/lib/*.php" ) as $lib_filename ) {
 /* ========== [ Include rtPanel Theme Options ] ========== */
 require_once( get_template_directory() . "/admin/rtp-theme-options.php" );
 
-
-//$args = array(
-//            'method' => 'POST',
-//            'headers' => array('content-type' => 'application/x-www-form-urlencoded' ),
-//            'httpversion' => '1.1',
-//            'body' => '<?xml version="1.0" encoding="utf-8" /?/>
-//
-//<YourMembership>
-//    <Version>1.65</Version>
-//    <ApiKey>3D638C5F-CCE2-4638-A2C1-355FA7BBC917</ApiKey>
-//    <CallID>001</CallID>
-//    <SessionID>A07C3BCC-0B39-4977-9E64-C00E918D572E</SessionID>
-//    <Call Method="Member.Profile.Get"></Call>
-//</YourMembership>',
-//        );
-//print_R(wp_remote_post('https://api.yourmembership.com', $args));
-
 // Nofollow external links only
 add_filter( 'the_content', 'rt_nofollow' );
 add_filter( 'the_excerpt', 'rt_nofollow' );
