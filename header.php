3d55a6b7b799ed6413e713f1aef081a47f34eea1
Joshua Abenazer <joshua.abenazer@rtcamp.com>
2011-08-01 09:15:43 +0000

    wp_title output changed

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
     <head>
         <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-        <title><?php wp_title('&laquo;', true, 'right'); ?><?php bloginfo('name'); ?></title>
+        <title><?php wp_title( '', true ); ?></title>
 
         <!-- Mobile Viewport Fix ( j.mp/mobileviewport & davidbcalhoun.com/2010/viewport-metatag ) -->
         <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" />
