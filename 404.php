82becebaf9a0bdace4aa45d80c8ec57e7b114955
Sagar Jadhav <sagar.jadhav@rtcamp.com>
2012-10-01 19:40:09 +0530

    HTML Markup Improved

--- 404.php
+++ 404.php
@@ -8,21 +8,20 @@
  */
 get_header(); ?>
 
-    <section id="content" role="main" class="rtp-grid-8 rtp-home-posts">
+    <section id="content" role="main" class="rtp-grid-8">
 
         <?php rtp_hook_begin_content(); ?>
-        
-        <h1 class="post-title rtp-main-title"><?php _e( 'Not Found', 'rtPanel' ); ?></h1>
-        <article id="post-0" class="rtp-not-found hentry">
+
+            <h1 class="post-title rtp-main-title"><?php _e( 'Not Found', 'rtPanel' ); ?></h1>
+
             <?php rtp_hook_begin_post(); ?>
 
-            <div class="post-content clearfix">
+            <div class="post-content clearfix rtp-not-found">
                 <p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'rtPanel' ); ?></p>
                 <?php get_search_form(); ?>
             </div>
 
             <?php rtp_hook_end_post();?>
-        </article><!-- #post-0 -->
 
         <?php rtp_hook_end_content(); ?>
 
