a62975b1bf27e6776e97fc6eee1bc08d742d254a
Faishal <saiyedfaishal@gmail.com>
2013-06-13 11:01:45 +0530

    replace old classes with foundation class

--- search.php
+++ search.php
@@ -10,7 +10,7 @@ get_header(); ?>
 
 <?php global $rtp_general; ?>
 
-    <section id="content" role="main" class="rtp-multiple-post<?php echo ( $rtp_general['search_code'] && $rtp_general['search_layout'] ) ? ' rtp-grid-12' : ' rtp-grid-8'; ?>"><!-- content begins --><?php
+    <section id="content" role="main" class="rtp-multiple-post<?php echo ( $rtp_general['search_code'] && $rtp_general['search_layout'] ) ? ' large-12' : ' large-8'; ?> columns"><!-- content begins --><?php
 
         rtp_hook_begin_content();
 
