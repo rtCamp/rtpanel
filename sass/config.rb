f9fe63cfc824e00adbf5c4ebb85a947dda336952
Faishal <saiyedfaishal@gmail.com>
2013-06-13 10:58:06 +0530

    config for compass

---
+++ sass/config.rb
@@ -0,0 +1,9 @@
+require 'zurb-foundation'
+http_path = "/"
+css_dir = "../"
+sass_dir = "."
+images_dir = "img"
+javascripts_dir = "js"
+output_style = :expanded
+relative_assets=true
+line_comments = false
