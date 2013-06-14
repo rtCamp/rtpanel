7c4249c8af4e6812da0c8db05874c4e0441c34e7
Faishal <saiyedfaishal@gmail.com>
2013-06-14 13:34:30 +0530

    pagging margin fix

--- lib/rtp-default-functions.php
+++ lib/rtp-default-functions.php
@@ -223,7 +223,7 @@ function rtp_default_archive_pagination() {
             if ( isset( $rtp_post_comments['pagination_show'] ) && $rtp_post_comments['pagination_show'] ) {
                 if ( ( $wp_query->max_num_pages > 1 ) ) { ?>
                 <div class="row">
-                    <nav class="wp-pagenavi"><?php
+                    <nav class="wp-pagenavi large-12 columns"><?php
                         echo paginate_links( array(
                                 'base' => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
                                 'format' => '?paged=%#%',
@@ -330,3 +330,243 @@ function rtp_custom_css(){
     echo ( $rtp_general['custom_styles'] ) ? '<style>' . $rtp_general['custom_styles'] . '</style>' . "\r\n" : '';
 }
 add_action( 'rtp_head', 'rtp_custom_css' );
+
+/**
+ * Galary Shorcode hijack with foundation
+ *
+ * @since rtPanel 3.2
+ */
+
+function rtp_galary_shorcode($output,$attr){
+    $post = get_post();
+    static $instance = 0;
+    $instance++;
+    if ( isset( $attr['orderby'] ) ) {
+        $attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
+        if ( !$attr['orderby'] )
+            unset( $attr['orderby'] );
+    }
+    extract(shortcode_atts(array(
+        'order'      => 'ASC',
+        'orderby'    => 'menu_order ID',
+        'id'         => $post->ID,
+        'itemtag'    => 'li',
+        'icontag'    => '',
+        'captiontag' => '',
+        'columns'    => 3,
+        'size'       => 'thumbnail',
+        'include'    => '',
+        'exclude'    => ''
+    ), $attr));
+
+    $id = intval($id);
+    if ( 'RAND' == $order )
+        $orderby = 'none';
+
+    if ( !empty($include) ) {
+        $_attachments = get_posts( array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+
+        $attachments = array();
+        foreach ( $_attachments as $key => $val ) {
+            $attachments[$val->ID] = $_attachments[$key];
+        }
+    } elseif ( !empty($exclude) ) {
+        $attachments = get_children( array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+    } else {
+        $attachments = get_children( array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+    }
+
+    if ( empty($attachments) )
+        return '';
+
+    if ( is_feed() ) {
+        $output = "\n";
+        foreach ( $attachments as $att_id => $attachment )
+            $output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+        return $output;
+    }
+
+    $itemtag = tag_escape($itemtag);
+    $captiontag = tag_escape($captiontag);
+    $icontag = tag_escape($icontag);
+    $valid_tags = wp_kses_allowed_html( 'post' );
+    if ( ! isset( $valid_tags[ $itemtag ] ) )
+        $itemtag = 'li';
+    if ( ! isset( $valid_tags[ $captiontag ] ) )
+        $captiontag = '';
+    if ( ! isset( $valid_tags[ $icontag ] ) )
+        $icontag = '';
+
+    $columns = intval($columns);
+    $itemwidth = $columns > 0 ? floor(100/$columns) : 100;
+    $float = is_rtl() ? 'right' : 'left';
+
+    $selector = "gallery-{$instance}";
+
+    $gallery_style = $gallery_div = '';
+   
+    $size_class = sanitize_html_class( $size );
+    $gallery_div = "<ul id='$selector' class='gallery galleryid-{$id} gallery-columns-{$columns} large-block-grid-{$columns} gallery-size-{$size_class} clearing-thumbs' data-clearing>";
+    $output = apply_filters( 'gallery_style', $gallery_style . "\n\t\t" . $gallery_div );
+
+    foreach ( $attachments as $id => $attachment ) {
+        $url = wp_get_attachment_image_src($id, "full");
+        $output .= "<{$itemtag} class='gallery-item'>";
+        $output .= "<a href='" . $url[0] . "'>".wp_get_attachment_image($id,'thumbnail', false, array("data-caption"=>  trim($attachment->post_excerpt)));
+        $output .= "</{$itemtag}>";
+    }
+
+    $output .= "</ul>\n";
+    return $output;
+}
+add_filter("post_gallery","rtp_galary_shorcode",1,2);
+
+/**
+ * Foundation Orbit Slider Funtion
+ * 
+ * @param WP_Query $slider_q
+ * @param type $slide_number
+ * @param type $content_length
+ * @param type $show_title
+ * @param type $show_excerpt
+ * @param type $show_navigation
+ * @param type $show_pagination
+ */
+function rtp_orbit_slider(WP_Query $slider_q=null,$slide_number = 100, $content_length = 200, $show_title = true, $show_excerpt = true, $show_navigation = true, $show_pagination = true){
+    if($slider_q==null){
+        $slider_q = new WP_Query( array( 'ignore_sticky_posts' => 1, 'posts_per_page' => $slide_number, 'order' => 'DESC' ) );
+    }
+    
+    $slider_image = '';
+    $slider_pagination = false;
+    $slider_html = '<div class="large-12 rtp-orbit-slider-row"><ul data-orbit id="rtp-orbit-slider" data-options="timer_speed:2500; bullets:false;">';
+
+    if ( $slider_q->have_posts() ) {
+        while ( $slider_q->have_posts() ) { $slider_q->the_post();
+            $image_details = wp_get_attachment_image_src( get_post_thumbnail_id(),"full" );
+            if ( $image_details) {
+                $slider_image = $image_details[0];
+            }else{
+                $slider_image=false;
+            }
+
+            if ( $slider_image ) {
+                    $slider_html .= '<li>';
+                    $slider_html .= '<img  src ="' . $slider_image . '" alt="' . esc_attr( get_the_title() ) . '" data-interchange="' . rtp_img_attachement_interchange_string(get_post_thumbnail_id(),"full","full","large") . '"/>';
+                    if($show_excerpt){
+                        $slider_html .= "<div class='orbit-caption'>";
+                        $slider_html .=  ( strlen( get_the_content() ) > $content_length )? wp_html_excerpt( get_the_content(), $content_length ) . '... ' : wp_html_excerpt( get_the_content(), $content_length );     
+                        $slider_html .="</div>";
+                        
+                    }
+                     
+                    $slider_html .= '</li>';
+            }
+
+            $slider_pagination = true;
+        }
+
+        
+    }
+    $slider_html .= '</ul></div>';
+
+    wp_reset_postdata();
+    echo $slider_html;
+     
+}
+
+function rtp_call_slider(){
+    if(is_home()){
+        $slider_q= new WP_Query(array("post_parent"=>1722,"post_status"=>"inherit","post_type"=>"any",'post_mime_type'=> 'image'));
+        rtp_orbit_slider($slider_q);
+    }
+}
+add_action("rtp_hook_begin_content_wrapper","rtp_call_slider",1,0);
+
+/**
+ * 
+ * @param type $ID
+ * @param type $large
+ * @param type $medium
+ * @param type $small
+ * @param type $custom = array({"path"=> '', "query"=> ''},{} ..) <br />
+ *      <table border=1>
+
+            <tr>
+              <th>Name</th>
+              <th>Media Query</th>
+            </tr>
+
+
+            <tr>
+              <td>default</td>
+              <td><code>only screen and (min-width: 1px)</code></td>
+            </tr>
+            <tr>
+              <td>small</td>
+              <td><code>only screen and (min-width: 768px)</code></td>
+            </tr>
+            <tr>
+              <td>medium</td>
+              <td><code>only screen and (min-width: 1280px)</code></td>
+            </tr>
+            <tr>
+              <td>large</td>
+              <td><code>only screen and (min-width: 1440px)</code></td>
+            </tr>
+            <tr>
+              <td>landscape</td>
+              <td><code>only screen and (orientation: landscape)</code></td>
+            </tr>
+            <tr>
+              <td>portrait</td>
+              <td><code>only screen and (orientation: portrait)</code></td>
+            </tr>
+            <tr>
+              <td>retina <small>(4.2.1)</small></td>
+              <td><code>only screen and (-webkit-min-device-pixel-ratio: 2),<br>
+                only screen and (min--moz-device-pixel-ratio: 2),<br>
+                only screen and (-o-min-device-pixel-ratio: 2/1),<br>
+                only screen and (min-device-pixel-ratio: 2),<br>
+                only screen and (min-resolution: 192dpi),<br>
+                only screen and (min-resolution: 2dppx)</code>
+              </td>
+            </tr>
+        </table>
+ * @return string
+ */
+function rtp_img_attachement_interchange_string($ID,$large=false,$medium=false,$small=false,$custom=array()){
+    $str = "";
+    $sep ="";
+    $img = wp_get_attachment_image_src($ID,"small");
+        if($img){
+            $str .= $sep . "[" . $img[0] . ",(default)]";
+            $sep=",";
+    }
+    if($small){
+        $img = wp_get_attachment_image_src($ID, $small);
+        if($img){
+            $str .= $sep . "[" . $img[0] . ",(small)]";
+            $sep=",";
+        }
+    }
+    if($medium){
+        $img = wp_get_attachment_image_src($ID, $medium);
+        if($img){
+            $str .= $sep .  "[" . $img[0] . ",(medium)]";
+            $sep=",";
+        }
+    }
+    if($large){
+        $img = wp_get_attachment_image_src($ID, $large);
+        if($img){
+            $str .= $sep . "[" . $img[0] . ",(large)]";
+            $sep=",";
+        }
+    }
+    foreach($custom as $sz){
+        $str .= $sep . "[" . $sz["path"] . ",(". $sz["query"]  . ")]";
+        $sep=",";
+    }
+    return $str;
+}
