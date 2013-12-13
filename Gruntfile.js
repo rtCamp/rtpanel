a6bc469a5ab10a63543bbd01cae5b0cbf87dc760
Manish Songirkar <manish.songirkar@rtcamp.com>
2013-12-13 13:27:29 +0530

    Added WordPress Deployment Support in Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,24 +2,26 @@
 module.exports = function(grunt) {
 
     // load all grunt tasks matching the `grunt-*` pattern
+    // Ref. https://npmjs.org/package/load-grunt-tasks
     require('load-grunt-tasks')(grunt);
 
     grunt.initConfig({
-
-        // watch for changes and trigger compass, jshint, uglify and livereload
+        
+        // Watch for hanges and trigger compass, jshint, uglify and livereload
+        // Ref. https://npmjs.org/package/grunt-contrib-watch
         watch: {
             compass: {
                 files: ['assets/rtpanel/**/*.{scss,sass}'],
                 tasks: ['compass']
             },
-            
             livereload: {
-                options: { livereload: true },
+                options: {livereload: true},
                 files: ['style.css', 'js/*.js', '*.html', '*.php', 'img/**/*.{png,jpg,jpeg,gif,webp,svg}']
             }
         },
-
-        // compass and scss
+        
+        // SCSS and Compass
+        // Ref. https://npmjs.org/package/grunt-contrib-compass
         compass: {
             dist: {
                 options: {
@@ -28,8 +30,9 @@ module.exports = function(grunt) {
                 }
             }
         },
-
-        // image optimization
+        
+        // Image Optimization
+        // Ref. https://npmjs.org/package/grunt-contrib-imagemin
         imagemin: {
             dist: {
                 options: {
@@ -37,46 +40,57 @@ module.exports = function(grunt) {
                     progressive: true
                 },
                 files: [{
-                    expand: true,
-                    cwd: 'img/',
-                    src: '**/*',
-                    dest: 'img/'
-                }]
+                        expand: true,
+                        cwd: 'img/',
+                        src: '**/*',
+                        dest: 'img/'
+                    }]
             }
         },
-
-        // deploy via rsync
-        deploy: {
+        
+        // WordPress Deployment
+        // Ref. https://npmjs.org/package/grunt-wordpress-deploy
+        wordpressdeploy: {
             options: {
-                src: "./",
-                args: ["--verbose"],
-                exclude: ['.git*', 'node_modules', '.sass-cache', 'Gruntfile.js', 'package.json', '.DS_Store', 'README.md', 'config.rb'],
-                recursive: true,
-                syncDestIgnoreExcl: true
+                backup_dir: "backups/",
+                rsync_args: ['--verbose', '--progress', '--archive', '--compress', '--omit-dir-times', '--delete'],
+                exclusions: ['Gruntfile.js', '.git/', 'tmp/*', 'backups/', 'wp-config.php', 'composer.json', 'composer.lock', 'README.md', '.gitignore', 'package.json', 'node_modules', '.sass-cache', 'npm-debug.log', '.scss-cache']
+            },
+            local: {
+                "title": "local",
+                "database": "database_name",
+                "user": "database_username",
+                "pass": "database_password",
+                "host": "database_host",
+                "url": "http://local_url",
+                "path": "/local_path"
             },
             staging: {
-                options: {
-                    dest: "~/path/to/theme",
-                    host: "user@host.com"
-                }
+                "title": "staging",
+                "database": "database_name",
+                "user": "database_username",
+                "pass": "database_password",
+                "host": "database_host",
+                "url": "http://staging_url",
+                "path": "/staging_path",
+                "ssh_host": "user@staging_host"
             },
-            production: {
-                options: {
-                    dest: "~/path/to/theme",
-                    host: "user@host.com"
-                }
+            final: {
+                "title": "staging",
+                "database": "database_name",
+                "user": "database_username",
+                "pass": "database_password",
+                "host": "database_host",
+                "url": "http://staging_url",
+                "path": "/staging_path",
+                "ssh_host": "user@staging_host"
             }
         }
 
     });
-
-    // rename tasks
-    grunt.renameTask('rsync', 'deploy');
-
-    // register task
-    grunt.registerTask('default', ['watch']);
     
-    // register task
-    grunt.registerTask('imagemin', ['imagemin']);
-
+    // Register Task
+    // Ref. http://gruntjs.com/api/grunt.task
+    grunt.task.registerTask(taskName, taskList);
+    task.registerTask('default', ['watch', 'imagemin', 'wordpressdeploy']);
 };
